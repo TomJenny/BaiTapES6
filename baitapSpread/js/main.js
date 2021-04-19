6fe3f12ca8c85f113ccd74b18c1ac07bb50330d9
QUACHTRUNGTIN <tin.quachtrung@gmail.com>
2021-04-19 12:49:21 +0700

    note

--- baitapSpread/js/main.js
+++ baitapSpread/js/main.js
@@ -1,1 +1,15 @@
-let str =
+let getELE = id => document.querySelector(id);
+
+let string = getELE(".heading").innerHTML.replace(" ", "");
+let characters = [...string];
+
+const renderSpanCharacters = (characters) => {
+    let content = '';
+    characters.map(char => {
+        content += `<span>${char}</span>`;
+    });
+
+    getELE(".heading").innerHTML = content;
+}
+
+renderSpanCharacters(characters);
